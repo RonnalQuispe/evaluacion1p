edce8c454c487200dbdc65e30e1c618299d1aaf5
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-11-01 16:09:24 -0500

    commit 3: segundo if si se cumple que el valor de X es entre 3 a 4

--- evaluacion.c
+++ evaluacion.c
@@ -3,18 +3,21 @@ int main(void){
     int x;
     printf("PROGRAMA PARA CALCULAR EL VALOR F(X)\n");  //inicio del programa//
     
-    while (x>=0)
+    while (x<0)
     {
         printf("Igrese el valor de X que sea numero positivo: ");
         scanf("%d\n", &x);
     }
-    
-        
-    
+   
     if (x>=0 && x<=2)
     {
-        printf("La funcion cuando x=%d es x^2 ", x);
+        printf("La funcion cuando x=%d es x^2\n ", x);
     }
+    if (x>=3 && x<=4)
+    {
+        printf("La funcion cuando x=%d es: -x^2+4\n ", x);
+    }
+    
     
     
     
