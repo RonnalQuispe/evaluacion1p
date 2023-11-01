170b4d135a63c16caa48f55434b8dd1f5374f2d3
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-11-01 15:48:36 -0500

    commit 2: condicional para que solo escoja numeros positivos

--- evaluacion.c
+++ evaluacion.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 int main(void){ 
     int x;
-    printf("PROGRAMA PARA CALCULAR EL VALOR F(X)\n");
-    printf("Igrese el valor de -x-:");
-    scanf("%d\n", &x);
-
+    printf("PROGRAMA PARA CALCULAR EL VALOR F(X)\n");  //inicio del programa//
+    
+    while (x>=0)
+    {
+        printf("Igrese el valor de X que sea numero positivo: ");
+        scanf("%d\n", &x);
+    }
+    
+        
+    
+    if (x>=0 && x<=2)
+    {
+        printf("La funcion cuando x=%d es x^2 ", x);
+    }
+    
+    
     
     return 0;
 }
