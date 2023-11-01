0501688f2881eb1aeaae8467f9f100d0dbe9458d
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-11-01 15:38:12 -0500

    commit 1: pide un valor de entrada X

--- evaluacion.c
+++ evaluacion.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
-int main(void){
+int main(void){ 
+    int x;
+    printf("PROGRAMA PARA CALCULAR EL VALOR F(X)\n");
+    printf("Igrese el valor de -x-:");
+    scanf("%d\n", &x);
+
+    
     return 0;
 }
